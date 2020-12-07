64c9a2bfcaa92263dff4c0c4eedc34b0bc946ddd
Phùng Văn Tú <phunvtuhaui@gmail.com>
2020-12-07 21:15:11 +0700

    up code base

---
+++ demo.multipedb/src/test/java/com/db2/demo/multipedb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db2.demo.multipedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
